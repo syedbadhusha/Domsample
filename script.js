874a3aa8a8e60d8227f4fe7eddaae861e379b18e
syedbadhusha <syedbadhusha21@gmail.com>
2024-02-20 12:11:54 +0530

    uploaded files for sample dom assignment

---
+++ script.js
@@ -0,0 +1,11 @@
+function userDate(){
+    var fName = document.getElementById('firstName').value;
+    var mName = document.getElementById('middleName').value;
+    var lName = document.getElementById('lastName').value;
+    var email = document.getElementById('email').value;
+
+    console.log(`First Name : ${fName}`)
+    console.log(`Middle Name : ${mName}`);
+    console.log(`Last Name : ${lName}`)
+    console.log(`Mail : ${email}`)
+}
